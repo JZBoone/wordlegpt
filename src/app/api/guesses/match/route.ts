af76659f4417edf010973ebac0e6a2345124ebd6
Zach Boone <zach.boone@fulfil.com>
2025-01-02 08:53:01 -0800

    refactor to use regex

--- src/app/api/guesses/match/route.ts
+++ src/app/api/guesses/match/route.ts
@@ -7,17 +7,13 @@ function validateParams(options: Record<string, unknown>):
       valid: true;
       options: {
         /**
-         * Defaults to 25
+         * Defaults to 5
          */
         limit: number;
         /**
-         * lower case
+         * Regex pattern
          */
-        pattern: string;
-        /**
-         * lower case
-         */
-        excludeLetters: string[];
+        pattern: RegExp;
       };
     }
   | { valid: false; reason: string } {
@@ -25,27 +21,16 @@ function validateParams(options: Record<string, unknown>):
   if (typeof pattern !== "string") {
     return { valid: false, reason: "Expected to receive a string" };
   }
-  if (pattern.length !== 5) {
-    return { valid: false, reason: "Pattern must be 5 characters long" };
+  let regexPattern: RegExp;
+  try {
+    regexPattern = new RegExp(pattern, "i");
+  } catch (e) {
+    console.error(`Invalid regex pattern: ${pattern}`);
+    console.error(e);
+    return { valid: false, reason: "Invalid regex pattern" };
   }
-  if (!/^[a-zA-Z*]{5}$/.test(pattern)) {
-    return {
-      valid: false,
-      reason: "Pattern can have only letters and asterisks",
-    };
-  }
-  const limit = Number(options.limit ?? 25);
-  const excludeLettersRaw = options.exclude ?? "";
-  if (typeof excludeLettersRaw !== "string") {
-    return { valid: false, reason: "Exclude must be a string" };
-  }
-  const excludeLetters = excludeLettersRaw.toLowerCase();
-  if (excludeLetters.length > 0 && !/^[a-z]{1,25}$/.test(excludeLetters)) {
-    return {
-      valid: false,
-      reason: "Exclude can have only letters and at most 25 characters",
-    };
-  }
+
+  const limit = Number(options.limit ?? 5);
 
   if (isNaN(limit) || limit < 1) {
     return { valid: false, reason: "Limit must be a positive number" };
@@ -54,8 +39,7 @@ function validateParams(options: Record<string, unknown>):
     valid: true,
     options: {
       limit,
-      pattern: pattern.toLowerCase(),
-      excludeLetters: excludeLetters.split(""),
+      pattern: regexPattern,
     },
   };
 }
@@ -63,29 +47,23 @@ function validateParams(options: Record<string, unknown>):
 /**
  * @swagger
  * /api/guesses/match:
- *   get:
- *     summary: Retrieve matching guesses based on the provided pattern and options.
+ *   post:
+ *     summary: Retrieve matching guesses based on the provided regex pattern.
  *     operationId: getMatchingGuesses
- *     parameters:
- *       - in: query
- *         name: pattern
- *         schema:
- *           type: string
- *         required: true
- *         description: A 5-character pattern where letters represent fixed positions and asterisks (*) represent wildcards.
- *       - in: query
- *         name: limit
- *         schema:
- *           type: integer
- *           default: 25
- *         required: false
- *         description: The maximum number of matches to return.
- *       - in: query
- *         name: exclude
- *         schema:
- *           type: string
- *         required: false
- *         description: A list of letters that cannot be in the word
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               pattern:
+ *                 type: string
+ *                 description: A regex pattern to match guesses. Letters should be lower case.
+ *               limit:
+ *                 type: integer
+ *                 default: 25
+ *                 description: The maximum number of matches to return.
  *     responses:
  *       200:
  *         description: A list of matching guesses.
@@ -109,14 +87,10 @@ function validateParams(options: Record<string, unknown>):
  *                   type: string
  *                   description: Error message
  */
-export async function GET(req: Request) {
-  const url = new URL(req.url);
+export async function POST(req: Request) {
+  const body = await req.json();
 
-  const validationResult = validateParams({
-    pattern: url.searchParams.get("pattern"),
-    limit: url.searchParams.get("limit"),
-    exclude: url.searchParams.get("exclude"),
-  });
+  const validationResult = validateParams(body);
 
   if (!validationResult.valid) {
     return NextResponse.json(
@@ -124,26 +98,15 @@ export async function GET(req: Request) {
       { status: 400 }
     );
   }
-  const { limit, pattern, excludeLetters } = validationResult.options;
+  const { limit, pattern } = validationResult.options;
 
   const answers = await loadAnswers();
 
   const matches = guesses.filter((guess) => {
-    for (let i = 0; i < 5; i++) {
-      if (answers.has(guess)) {
-        return false;
-      }
-      if (excludeLetters.includes(guess[i])) {
-        return false;
-      }
-      if (pattern[i] === "*") {
-        continue;
-      }
-      if (pattern[i] !== guess[i]) {
-        return false;
-      }
+    if (answers.has(guess)) {
+      return false;
     }
-    return true;
+    return pattern.test(guess);
   });
 
   return NextResponse.json({
